9518069f5a125558f16584e5af7d9f48d40ff0e2
yanzhen huang <huangyanzhen0108@163.com>
2025-10-20 01:36:54 +0800

    feat: Finish suffix array program

--- ch2_suffix_arr.cpp
+++ ch2_suffix_arr.cpp
@@ -68,7 +68,7 @@ vector<pair<int, int>> flatten_linked_list(
     return flattened_linked_list;
 }
 
-int sort_pairs(
+vector<pair<int, int>> sort_pairs(
     vector<pair<int, int>> pair_list,
     int max_value
 ){
@@ -98,11 +98,11 @@ int sort_pairs(
         pair_list.size()
     );
     
-    for (int i=0; i<first_sorted_pair_list.size(); i++){
-        cout << first_sorted_pair_list[i].first << " " << first_sorted_pair_list[i].second << endl;
-    }
+    // for (int i=0; i<first_sorted_pair_list.size(); i++){
+    //     cout << first_sorted_pair_list[i].first << " " << first_sorted_pair_list[i].second << endl;
+    // }
 
-    return 0;
+    return first_sorted_pair_list;
 }
 
 int main(){
@@ -110,8 +110,71 @@ int main(){
 
     // long long string_size = sample.size();
 
-    sort_pairs(
-        {{1, 2}, {2, 1}, {1, 3}, {2, 3}, {1, 4}, {2, 4}, {1, 5}, {2, 5}, {1, 6}, {2, 6}},
-        6
-    );
+    string sample;
+    cin >> sample;
+
+
+    int num_stages = 1;
+    int length = sample.size();
+    int _length = length;
+    while(_length > 0){
+        _length = _length >> 1;
+        num_stages++;
+    }
+    int pad_length = (1 << num_stages) - length;
+    sample += string(pad_length, '$');
+    length = sample.size();
+
+    vector<pair<int, int>> pair_list;
+    map<string, int> substring_map;
+
+    // Convert char to int to sort
+    map<int, int> set;
+    for(int i=0; i<length; i++){
+        if (set.find((int) sample[i]) != set.end()) continue;
+        pair_list.push_back((pair<int, int>){sample[i], i});
+        set[(int) sample[i]] = i;
+    }
+
+    pair_list = sort_pairs(pair_list, 200);
+    
+    // Convert int back to char, then to string.
+    for(int i=0; i<pair_list.size(); i++){
+        pair<int, int> this_pair = pair_list[i];
+        char c = this_pair.first;
+        string s = string(1, c);
+        substring_map[s] = i + 1;
+    }
+
+
+    for (int i=1; i<num_stages + 1; i++){
+        vector<pair<int, int>> stage_pair_list;
+        map<pair<int, int>, string> substring_pair_map;
+
+        for(int j=0; j<length - (1 << i); j++){
+            string substring = sample.substr(j, (1 << i));
+            int first_half = substring_map[
+                substring.substr(0, substring.size() >> 1)];
+            int second_half = substring_map[substring.substr(
+                substring.size() >> 1, 
+                substring.size() >> 1)];
+            if (
+                substring_pair_map.find({first_half, second_half}) != substring_pair_map.end()
+            ) continue;
+
+            stage_pair_list.push_back((pair<int, int>){first_half, second_half});
+            substring_pair_map.insert({(pair<int, int>){first_half, second_half}, substring});
+        }
+
+        vector<pair<int, int>> sorted_pair_list = sort_pairs(stage_pair_list, length - num_stages);
+        
+        vector<string> sorted_substrings;
+        for(int j=0; j<sorted_pair_list.size(); j++){
+            cout << j + 1 << " "<< substring_pair_map[sorted_pair_list[j]] << " => " << sorted_pair_list[j].first << " " << sorted_pair_list[j].second << endl;
+            sorted_substrings.push_back(substring_pair_map[sorted_pair_list[j]]);
+            substring_map.insert({
+                substring_pair_map[sorted_pair_list[j]], j + 1});
+        }
+        cout << "\n";
+    }
 }
